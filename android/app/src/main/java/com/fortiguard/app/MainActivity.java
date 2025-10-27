10235eab447a8b515e829eeb03d0cf05b1ceda16
i91480905-tech <i91480905@gmail.com>
2025-10-27 04:36:53 -0700

    Update MainActivity.java

--- android/app/src/main/java/com/fortiguard/app/MainActivity.java
+++ android/app/src/main/java/com/fortiguard/app/MainActivity.java
@@ -1,1 +1,27 @@
-��$j����b��t��
+package com.fortiguard.app;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
+
+// This is the main entry point for the Android part of your application.
+// It extends Capacitor's BridgeActivity, which hosts the web view.
+public class MainActivity extends BridgeActivity {
+
+  // The onCreate method is called when the app's main activity is first created.
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // This is the most critical part.
+    // It initializes the Capacitor bridge and registers all custom native plugins.
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // By adding EmergencyPlugin.class here, we are telling Capacitor:
+      // "When the JavaScript code calls for a plugin named 'Emergency',
+      // use the code found in our EmergencyPlugin.java file."
+      // Without this line, the app would crash, saying the plugin is not implemented.
+      add(EmergencyPlugin.class);
+    }});
+  }
+}
